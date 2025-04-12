c838bf89795e070098e1719574af89cd53d98474
balanayj <jbalanay.rose934@gmail.com>
2025-04-12 02:58:25 -0400

    implement check number function

--- script.js
+++ script.js
@@ -6,20 +6,21 @@ function getRandomNumber() {
 }
 
 const randomNum = getRandomNumber();
-console.log("Number:", randomNum);
+console.log(randomNum);
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
-let recognition = new window.SpeechRecognition();
+const recognition = new window.SpeechRecognition();
 
 // Start recognition and game
 recognition.start();
 
 // Capture user speech
 function onSpeak(event) {
-  const msg = event.result[0][0].transcript; // You can log the event to view the structure of the data
-  console.log(msg);
+  const msg = event.results[0][0].transcript;
+  writeMessage(msg);
+  checkNumber(msg);
 }
 
 // Listen to and handle the speech event
@@ -35,3 +36,83 @@ function writeMessage(msg) {
 
   msgEl.append(div, span);
 }
+
+// Check msg against the secret number
+function checkNumber(msg) {
+
+  const wordToNumber = {
+    one: 1,
+    won: 1,
+    two: 2,
+    to: 2,
+    too: 2,
+    three: 3,
+    four: 4,
+    for: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    ate: 8,
+    nine: 9,
+    ten: 10,
+  };
+
+  if (wordToNumber[msg]) {
+    console.log(`adjusting ${msg} to ${wordToNumber[msg]}`);
+    msg = wordToNumber[msg];
+  } // Convert to number after adjustments
+
+  const num = Number(msg);
+
+  // Check if the spoken content is a valid number
+  if (Number.isNaN(num)) {
+    const div = document.createElement("div");
+    div.textContent = "That is not a valid number";
+    msgEl.innerHTML = "";
+    msgEl.append(div);
+    return;
+  }
+
+  // Check if it's in range
+  if (num < 1 || num > 100) {
+    const div = document.createElement("div");
+    div.textContent = "Number must be between 1 and 100";
+    msgEl.innerHTML = "";
+    msgEl.append(div);
+    return;
+  }
+
+  // Check the number and provide feedback
+  if (num === randomNum) {
+    const h2 = document.createElement("h2");
+    h2.textContent = `Congrats! You have guessed the number! It was ${num}`;
+
+    const button = document.createElement("button");
+    button.classList.add("play-again");
+    button.id = "play-again";
+    button.textContent = "Play Again";
+    // Add listener and handler to button
+    button.addEventListener("click", () => window.location.reload());
+
+    // Clear out innerHTML of msgEl
+    msgEl.innerHTML = "";
+    msgEl.append(h2, button);
+  } else if (num > randomNum) {
+    const div = document.createElement("div");
+    div.textContent = "GO LOWER";
+
+    msgEl.innerHTML = "";
+    msgEl.append(div);
+  } else {
+    // if (num < randomNum)
+    const div = document.createElement("div");
+    div.textContent = "GO HIGHER";
+
+    msgEl.innerHTML = "";
+    msgEl.append(div);
+  }
+}
+
+// At the end of the SpeechRecognition service, start it again
+recognition.addEventListener("end", () => recognition.start());
